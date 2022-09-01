7bf2d5854fc3fde7dc8a311d7a961d2d22367cf0
alex <aa.santos95@gmail.com>
2022-09-01 15:25:07 -0300

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = ''
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'loja/static/loja')
 STATIC_URL = 'loja/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
