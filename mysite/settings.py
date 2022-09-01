dc17547472a812597eee1770aac54e26f86f5201
alex <aa.santos95@gmail.com>
2022-09-01 15:06:47 -0300

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,8 +130,8 @@ MEDIA_URL = ''
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'loja/static')
-STATIC_URL = 'loja/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'loja/static/loja')
+STATIC_URL = 'loja/static/loja'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'loja/static/loja'),
 )
