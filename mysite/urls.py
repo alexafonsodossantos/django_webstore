cf5bb1d3d76a165610ab3a5eae625f8f81d51b6b
alex <aa.santos95@gmail.com>
2022-09-01 14:44:31 -0300

    changed urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loja/', include('loja.urls', namespace="loja")),
+    path('', include('loja.urls', namespace="loja")),
     path('accounts/', include("django.contrib.auth.urls")),
     
 
