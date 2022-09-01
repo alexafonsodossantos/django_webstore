80dad9296e64a54d346c6dd5e1fb60f5d13551c9
alex <aa.santos95@gmail.com>
2022-09-01 14:04:20 -0300

    fixed static dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,19 @@ STATIC_URL = 'loja/static/'
 MEDIA_ROOT = ''
 MEDIA_URL = ''
 
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'loja/static')
+STATIC_URL = 'loja/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'loja/static/loja'),
+)
+
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
