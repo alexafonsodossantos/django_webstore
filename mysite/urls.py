f82c6a51256d4a205463cc0988254547dcab1c9d
alexafonsodossantos <aa.santos95@gmail.com>
2022-09-01 11:47:43 -0300

    moved from old python repo to this new one

---
+++ mysite/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('loja/', include('loja.urls', namespace="loja")),
+    path('accounts/', include("django.contrib.auth.urls")),
+    
+
+
+]
