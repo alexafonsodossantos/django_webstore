c14e106d1a850c6f46ea588bf1865696fa29860b
alex <aa.santos95@gmail.com>
2022-09-01 15:20:38 -0300

    added whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -122,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'loja/static/'
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
@@ -130,11 +130,9 @@ MEDIA_URL = ''
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'loja/static/loja')
-STATIC_URL = 'loja/static/loja'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'loja/static/loja'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'loja/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
